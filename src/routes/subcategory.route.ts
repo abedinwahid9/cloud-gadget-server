55b1847c3a6e6c31128be5ee8273c1e805318dcb
abedinwahid9 <abedinwahid9@gmail.com>
2025-10-22 12:49:58 +0600

    sub category get all data api complete

--- src/routes/subcategory.route.ts
+++ src/routes/subcategory.route.ts
@@ -2,10 +2,12 @@ import express from "express";
 import {
   createSubCategory,
   deleteSubCategoryById,
+  getAllSubCategory,
 } from "../controllers/subcategory.controller";
 
 const router = express.Router();
 
+router.get("/", getAllSubCategory);
 router.post("/", createSubCategory);
 router.delete("/:id", deleteSubCategoryById);
 
