ae93bdf4f46c476eb2fcb9971ec457113514577f
Abedin Wahid <94265191+abedinwahid9@users.noreply.github.com>
2025-11-30 01:37:41 +0600

    Update CORS origin to include new Netlify URL

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://cloud-gadget-server.onrender.com",
+      "https://cloudiegadget.netlify.app",
     ],
     credentials: true,
   })
