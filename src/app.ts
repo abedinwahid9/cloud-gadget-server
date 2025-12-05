db4f265b1ad31e7ff8c13faec5e9aef20e688e88
abedinwahid9 <abedinwahid9@gmail.com>
2025-12-06 01:25:59 +0600

    some cookies bug fix

--- src/app.ts
+++ src/app.ts
@@ -7,18 +7,18 @@ var cookieParser = require("cookie-parser");
 const app = express();
 
 // Middlewares
+app.use(cookieParser());
 app.use(express.json({ limit: "1000mb" }));
 app.use(express.urlencoded({ extended: true, limit: "1000mb" }));
 app.use(helmet());
 app.use(
   cors({
     origin: ["http://localhost:3000", "https://cloudiegadget.netlify.app"],
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true, // ← essential
   })
 );
+
 app.use(express.static("public"));
-app.use(cookieParser());
 
 // route
 app.use("/api/v1", router);
